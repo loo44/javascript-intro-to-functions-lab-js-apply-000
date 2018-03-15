60b4107c90af091ad4434809efb31f22aa3e444d
Luis Ruiz <giosterr44@gmail.com>
2018-03-15 14:06:48 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return shout(string).toLowerCase();
+}
+
+function logShout(string) {
+  console.log("hello".toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log("HELLO".toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string.toLowerCase)
+   {
+   return ("I can\'t hear you!");
+ }
+}
+
+function sayHiToGrandma(string) {
+ if (string.toUpperCase)
+{
+   return ("YES INDEED!");
+ }
+}
+
+function sayHiToGrandma(string) {
+if  (string==="I love you, Grandma.")
+ {
+   return ("I love you, too.")
+ }
+}
+
+
